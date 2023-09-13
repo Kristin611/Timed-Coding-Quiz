b13ae4095f762bc5e9b68851d7f7fac51b6fde5f
Kris10 <kmrichie611@gmail.com>
2023-09-12 22:18:18 -0500

    added displayQuestion function

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,8 @@ const question = document.getElementById('question-title')
 const answerList = document.getElementById('answer-list')
 const startBtn = document.getElementById('startBtn')
 
+    let mcIndex = 0
+
 const mcQuestions = [
     {
         question: 'Which of the following is not a primitive data type?',
@@ -15,7 +17,48 @@ const mcQuestions = [
         choices: ['Now or Never', 'Null', 'Not Another', 'Not a Number'],
         correctAnswer: 3,
     }
-]
+
+];
+    
+    function mcStart() {
+        startTime()
+        displayQuestion()
+    }
+
+    
+
+   
+
+function displayQuestion() {
+   const questionTitle = mcQuestions[mcIndex].question;
+   question.textContent = questionTitle; 
+   
+    const mcChoices = mcQuestions[mcIndex].choices;
+
+    let mcChoicesEl = ''
+   for (var i = 0; i < mcChoices.length; i++) {
+         mcChoicesEl += `<li>${mcChoices[i]}</li>`
+         answerList.innerHTML = mcChoicesEl 
+    // console.log(`Question: ${mcQuestions[i].question}`); //
+    // console.log(`Choices: ${mcQuestions[i].choices}`);
+    // console.log(`Correct Answer: ${mcQuestions[i].correctAnswer}`)
+    } 
+
+    answerList.addEventListener('click', function(event){
+        event.preventDefault()
+        if (event.target.matches('li')) {
+            console.log(event.target.textContent)
+            mcChecker(event.target.textContent)
+        }
+    })
+}
+
+function mcChecker(mcContent) {
+    //mcIndex++ to jump to the next question
+    mcIndex++
+    console.log(mcIndex)
+    displayQuestion()
+}
 
 let timer;
 let timerLeft = 5;
@@ -47,4 +90,4 @@ function stopTimer() {
 
 
 
-startBtn.addEventListener('click', startTime);
+startBtn.addEventListener('click', mcStart);
